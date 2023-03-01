8d512d079ae41e1b8841fc62b3234c9c0b0ed66b
Finn Lambert <107528359+FinnLambert465@users.noreply.github.com>
2023-03-01 15:22:03 +1100

    Edited Spawner script

--- Assets/Scripts/GameSpawner.cs
+++ Assets/Scripts/GameSpawner.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class GameSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] theEnemy;
-    [SerializeField]
-    Collider[] theCollider;
-    [SerializeField] float radius = 0f;
+
 
 
     [SerializeField] float enemyCount;
@@ -15,7 +13,6 @@ public class GameSpawner : MonoBehaviour
     private void Start()
     {
         StartCoroutine(EnemyDrop());
-        theCollider = Physics.OverlapSphere (transform.position, radius);
     }
 
     private IEnumerator EnemyDrop()
@@ -30,10 +27,6 @@ public class GameSpawner : MonoBehaviour
             {
                 if (enemyPicker == i)
                 {
-                    //bool canSpawnHere;
-
-                    //canSpawnHere = PreventSpawnerOverlap(spawnPos);
-
                     Instantiate(theEnemy[i], new Vector3(xPos, 2, zPos), Quaternion.identity);
                     yield return null;
 
@@ -45,35 +38,4 @@ public class GameSpawner : MonoBehaviour
 
         }
     }
-
-
-
-   //private void PreventSpawnerOverlap(Vector3 spawnPos)
-   // {
-   //     for (int i = 0; i < theEnemy.Length; i++)
-   //     {
-   //         theCollider = Physics.OverlapSphere(transform.position, radius);
-   //         for (int i = 0; i < theCollider.Length; i++)
-   //         {
-   //             Vector3 centerPoint = theCollider.bounds.center;
-   //             float width = theCollider.bounds.extents.x;
-   //             float lenghth = theCollider.bounds.extents.x;
-   //             float hight = 5;
-
-   //             float leftExtent = centerPoint.x - width;
-   //             float rightExtent = centerPoint.x + width;
-   //             float lowerExtent = centerPoint.z - width;
-   //             float upperExtent = centerPoint.z + width;
-
-   //             if (spawnPos.x >= leftExtent && spawnPos.x <= rightExtent)
-   //             {
-   //                 if (spawnPos.y >= lowerExtent && spawnPos.z <= upperExtent)
-   //                 {
-   //                     return false;
-   //                 }
-   //                 return false;
-   //             }
-   //         }
-   //     }
-   // }
 }
